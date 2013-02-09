16e9d2e034f865b2ac8be497194e64c33a84d592
Chris Olds <chrisolds22@gmail.com>
2013-02-09 14:45:59 -0600

    heroku precompile db fix

--- config/application.rb
+++ config/application.rb
@@ -72,5 +72,9 @@ module Recommender
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #for Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
